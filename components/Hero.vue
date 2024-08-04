7d9cdfd7298c20738eb6e2fd3dd27e9ba614af85
Shubhankamin <aminshubhank@gmail.com>
2024-08-04 19:22:14 +0530

    new website

---
+++ components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-container fluid style="background-color: black">
+      <v-row>
+        <v-col cols="6" class="d-flex pt-16 flex-column align-center">
+          <div class="pt-14">
+            <h1 class="heading text-white w-50">hi, i am</h1>
+            <h1 class="heading text-white">robert garcia.</h1>
+          </div>
+        </v-col>
+        <v-col cols="6">
+          <div class="bg-grey h-50">
+            <v-img src="/images/me-1.jpg"> </v-img>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<style scoped>
+.heading {
+  text-transform: uppercase;
+}
+</style>
---
+++ components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-container class="main" fluid>
+      <v-row>
+        <v-col class="d-flex justify-space-between">
+          <div>
+            <p>SHUBHANK</p>
+          </div>
+          <div class="d-flex ga-5">
+            <p>WORK</p>
+            <p>ABOUT</p>
+            <p>CONTACT</p>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<style scoped>
+
+</style>
